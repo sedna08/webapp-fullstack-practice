538f319ff518789fb0dda5e8e577404fb03c5add
sedna08 <ochiamitcham@gmail.com>
2024-09-27 23:07:24 +0800

    Added starting server code

--- backend/index.js
+++ backend/index.js
@@ -94,3 +94,7 @@ app.delete('/users/:id', async(req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+// start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
